6edea7189b5cadd785445a83ef4b313de1241de9
dresssmrt <dresssmrt@gmail.com>
2023-08-04 14:06:03 +0000

    initial project setup

---
+++ notification-service/src/test/java/com/tech_tribe/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech_tribe.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
